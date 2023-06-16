1d00036a4d98ffdb365b82cb89cdde80f2cd5d3d
Paradoxtec <philemonolakitan@gmail.com>
2023-06-15 18:59:02 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <unistd.h>
-
 /**
  * main - Entry point
  *
  * Description: A C program that demonstrates the usage of the write function
  * to write a specific message to the standard error stream.
- *
- * Return: Always 1
+ * Return: 1 (success)
  */
 
-	int main(void)
+int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
