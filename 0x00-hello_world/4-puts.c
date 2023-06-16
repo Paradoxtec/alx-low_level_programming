6bcb8e16d0e65b4a92869fbcad125cc2fd850940
Paradoxtec <philemonolakitan@gmail.com>
2023-06-15 17:32:51 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
 /**
- * main . A programe that print a line with puts function
+ * main - Entry point
+ *
+ * Description: A programe that print a line with puts function.
  * Return: 0 (Success)
+ *
  */
+
+#include <stdio.h>
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
